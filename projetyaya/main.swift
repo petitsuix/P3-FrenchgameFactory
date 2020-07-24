b5e9f57e45bd1afd9bcc63da6a4c27d4733023db
Ianis Richardier <ianis.richardier@gmail.com>
2020-07-24 13:16:48 +0200

    Initial Commit

---
+++ projetyaya/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  projetyaya
+//
+//  Created by Richardier on 24/07/2020.
+//  Copyright © 2020 Richardier. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
