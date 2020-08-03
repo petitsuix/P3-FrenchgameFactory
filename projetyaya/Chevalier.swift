d11516d381489b6f6c9e1d422104e4669fbc6832
Ianis Richardier <ianis.richardier@gmail.com>
2020-08-03 13:18:45 +0200

    first commit

---
+++ projetyaya/Chevalier.swift
@@ -0,0 +1,9 @@
+//
+//  Chevalier.swift
+//  P3-Frenchgamefactory
+//
+//  Created by Richardier on 29/07/2020.
+//  Copyright © 2020 Richardier. All rights reserved.
+//
+
+import Foundation
---
+++ projetyaya/Sorcière.swift
@@ -0,0 +1,9 @@
+//
+//  Sorcière.swift
+//  P3-Frenchgamefactory
+//
+//  Created by Richardier on 29/07/2020.
+//  Copyright © 2020 Richardier. All rights reserved.
+//
+
+import Foundation
